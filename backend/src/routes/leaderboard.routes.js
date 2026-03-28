d8299fc80574428d77e8612cd5173d55c5435924
priyabratasahoo780 <priyabrata.sahoo.cg@gmail.com>
2026-03-29 01:05:15 +0530

    Elite UI: Global Premium Synchronization and Engineer OS Overhaul

--- backend/src/routes/leaderboard.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getLeaderboard } = require('../controllers/leaderboard.controller');
-
-const router = express.Router();
-
-router.get('/', getLeaderboard);
-
-module.exports = router;
